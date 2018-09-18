ef4745e0c0158ac1f47ccfd3f4f89fdc8299a9b5
BarnaTB <barnabastb2@gmail.com>
2018-09-18 18:20:08 +0300

    ft(User should be able to delete a task from a to-do list): Commit to deliver a feature that enables a user to delete a specific task from a to-do list. [Deliver #160581171]

--- src/accounts.py
+++ src/accounts.py
@@ -6,7 +6,9 @@ def add_account(name, password):
         print('A name should only contain alphabetical characters!')
     else:
         accounts[password] = name
-        print('{} is registered. Go ahead and login'.format(accounts[password]))
+        print(
+            '{} is registered. Go ahead and login'.format(accounts[password])
+            )
         return accounts
 
 
--- src/tasks.py
+++ src/tasks.py
@@ -9,3 +9,12 @@ def create_task(task):
     todo_list.append(task)
     print('Your list: {0}'.format(todo_list))
     return todo_list
+
+
+def delete_task(task):
+    for a_task in todo_list:
+        if a_task == task:
+            todo_list.remove(task)
+            print(todo_list)
+            return todo_list
+        print('That task does not exist in this list.')
