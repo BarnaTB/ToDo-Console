eff04ea24255e7cdb0c00c4f9309036349ad10d6
BarnaTB <barnabastb2@gmail.com>
2018-09-18 18:02:54 +0300

    ft(User should be able to create a task): Commit delivers a feature that enables a logged in user to add a task to a list. [Deliver #160581133]

---
+++ src/tasks.py
@@ -0,0 +1,11 @@
+todo_list = []
+
+
+def create_task(task):
+    if not task or task.isspace():
+        print('Please enter a task')
+    if not task.isalnum():
+        print('A task should be a string')
+    todo_list.append(task)
+    print('Your list: {0}'.format(todo_list))
+    return todo_list
