f0ac7105332c79fa0d21ec0551bbc9dfed98d5fe
BarnaTB <barnabastb2@gmail.com>
2018-09-18 18:47:28 +0300

    ft(User should be able to mark a task as finished): Commit delivers a feature that enables a user to mark a specific task that has been finished as finished. [Deliver #160581218]

--- src/tasks.py
+++ src/tasks.py
@@ -18,3 +18,14 @@ def delete_task(task):
             print(todo_list)
             return todo_list
         print('That task does not exist in this list.')
+
+
+def mark_as_finished(task):
+    for a_task in todo_list:
+        if a_task == task:
+            finished_task = "{0} -finished".format(a_task)
+            todo_list.remove(a_task)
+            todo_list.append(finished_task)
+            print(todo_list)
+            return todo_list
+        print('This task does not exist in your list')
